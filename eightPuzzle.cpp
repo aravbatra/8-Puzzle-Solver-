40e327ad0b340908e11ba73c98e8373007b8ce35
aravbatra <arav.batra@gmail.com>
2017-11-03 04:32:19 -0700

    added all heuristic searches

--- eightPuzzle.cpp
+++ eightPuzzle.cpp
@@ -307,15 +307,317 @@ void puzzle::uniformCostSearch(path* x){
 
 }
 void puzzle::misplacedTileSearch(path* x){
-	
-}
-void puzzle::manhattanDistanceSearch(path* x){
+	int heuristicMisplaced = 0;
+	vector< vector<char> > solvedPuzzle;
+	vector<char> row1, row2, row3;
+	row1.push_back('1');
+	row1.push_back('2');
+	row1.push_back('3');
+
+	row2.push_back('4');
+	row2.push_back('5');
+	row2.push_back('6');
+
+	row3.push_back('7');
+	row3.push_back('8');
+	row3.push_back('0');
+	solvedPuzzle.push_back(row1);
+	solvedPuzzle.push_back(row2);
+	solvedPuzzle.push_back(row3);
+
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			if(x->puzzle.at(i).at(j) != solvedPuzzle.at(i).at(j))
+			{
+				heuristicMisplaced++;
+			}
+		}
+	}
+	x->heuristic = heuristicMisplaced;
+	int horizontalIndex = 0;
+	int verticalIndex = 0;
+
+	vector< vector<char> > leftShift;
+	vector< vector<char> > rightShift;
+	vector< vector<char> > downShift;
+	vector< vector<char> > upShift;
+
 	if(isGoalState(x->puzzle)){
-		cout << "LITTY BOIZ" << endl;
+		goalState = true;
+		goalNode = x;
 	}
 	else{
-		cout << "Fuck" << endl;
+		q.pop();
 	}
+	for(int i =0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			if(charToInt(x->puzzle.at(i).at(j)) == 0){
+				horizontalIndex = i;
+				verticalIndex = j;
+				break;
+			}
+		}
+	}
+	int nOfChildren = childrenCalculator(x);
+	bool left = true;
+	bool right = true;
+	bool up = true;
+	bool down = true;
+	for(int i = 0; i < x->illegalMoves.size(); i++){
+		if(x->illegalMoves.at(i) == 1){
+			left = false;
+		}
+		else if(x->illegalMoves.at(i) == 2){
+			right = false;
+		}
+		if(x->illegalMoves.at(i) == 3){
+			up = false;
+		}
+		else if(x->illegalMoves.at(i) == 4){
+			down = false;
+		}
+	}
+
+	if(left){
+		leftShift = moveLeft(x,horizontalIndex,verticalIndex);
+		assignChild(x, leftShift, x->child1);
+	}
+	if(right){
+		rightShift = moveRight(x,horizontalIndex,verticalIndex);
+		assignChild(x, rightShift, x->child2);
+	}
+	if(up){
+		upShift = moveUp(x,horizontalIndex,verticalIndex);
+		assignChild(x, upShift, x->child3);
+	}
+	if(down){
+		downShift = moveDown(x,horizontalIndex,verticalIndex);
+		assignChild(x, downShift, x->child4);
+	}
+
+	if(goalState){
+		while(q.top() != goalNode)
+		{
+			q.pop();
+		}
+		displayPuzzle(q.top());
+		cout << "Goal!" << endl;
+
+		cout << "To solve this problem the search algorithm expanded a total of ";
+		cout << totalNodes << " nodes." << endl;
+
+		cout << "The maximum nodes in the queue at any one time was ";
+		cout << nodesInQueue << "." << endl;
+
+		cout << "The depth of the goal node was ";
+		cout << q.top()->branchCost << "." << endl;
+
+		return;
+	}
+
+	//Left = 1
+	//Right = 2
+	//Up = 3
+	//Down = 4
+
+	misplacedTileSearch(q.top());
+}
+void puzzle::manhattanDistanceSearch(path* x){
+	int heuristicDistance = 0;
+	vector< vector<char> > solvedPuzzle;
+	vector<char> row1, row2, row3;
+	row1.push_back('1');
+	row1.push_back('2');
+	row1.push_back('3');
+
+	row2.push_back('4');
+	row2.push_back('5');
+	row2.push_back('6');
+
+	row3.push_back('7');
+	row3.push_back('8');
+	row3.push_back('0');
+	solvedPuzzle.push_back(row1);
+	solvedPuzzle.push_back(row2);
+	solvedPuzzle.push_back(row3);
+
+	vector<int> one;
+	one.push_back(0);
+	one.push_back(0);
+	vector<int> two;
+	two.push_back(0);
+	two.push_back(1);
+	vector<int> three;
+	three.push_back(0);
+	three.push_back(2);
+	vector<int> four;
+	four.push_back(1);
+	four.push_back(0);
+	vector<int> five;
+	five.push_back(1);
+	five.push_back(1);
+	vector<int> six;
+	six.push_back(1);
+	six.push_back(2);
+	vector<int> seven;
+	seven.push_back(2);
+	seven.push_back(0);
+	vector<int> eight;
+	eight.push_back(2);
+	eight.push_back(1);
+	vector<int> zero;
+	zero.push_back(2);
+	zero.push_back(2);
+
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			if(x->puzzle.at(i).at(j) != solvedPuzzle.at(i).at(j))
+			{
+				int misplacedValue = charToInt(x->puzzle.at(i).at(j));
+				switch (misplacedValue) {
+					case 0:{
+						heuristicDistance += abs(zero.at(0) - i);
+						heuristicDistance += abs(zero.at(1) - j);
+						break;
+					}
+					case 1:{
+						heuristicDistance += abs(one.at(0) - i);
+						heuristicDistance += abs(one.at(1) - j);
+						break;
+					}
+					case 2:{
+						heuristicDistance += abs(two.at(0) - i);
+						heuristicDistance += abs(two.at(1) - j);
+						break;
+					}
+					case 3:{
+						heuristicDistance += abs(three.at(0) - i);
+						heuristicDistance += abs(three.at(1) - j);
+						break;
+					}
+					case 4:{
+						heuristicDistance += abs(four.at(0) - i);
+						heuristicDistance += abs(four.at(1) - j);
+						break;
+					}
+					case 5:{
+						heuristicDistance += abs(five.at(0) - i);
+						heuristicDistance += abs(five.at(1) - j);
+						break;
+					}
+					case 6:{
+						heuristicDistance += abs(six.at(0) - i);
+						heuristicDistance += abs(six.at(1) - j);
+						break;
+					}
+					case 7:{
+						heuristicDistance += abs(seven.at(0) - i);
+						heuristicDistance += abs(seven.at(1) - j);
+						break;
+					}
+					case 8:{
+						heuristicDistance += abs(eight.at(0) - i);
+						heuristicDistance += abs(eight.at(1) - j);
+						break;
+					}
+
+				}
+
+			}
+		}
+	}
+	x->heuristic = heuristicDistance;
+	int horizontalIndex = 0;
+	int verticalIndex = 0;
+
+	vector< vector<char> > leftShift;
+	vector< vector<char> > rightShift;
+	vector< vector<char> > downShift;
+	vector< vector<char> > upShift;
+
+	if(isGoalState(x->puzzle)){
+		goalState = true;
+		goalNode = x;
+	}
+	else{
+		q.pop();
+	}
+	for(int i =0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			if(charToInt(x->puzzle.at(i).at(j)) == 0){
+				horizontalIndex = i;
+				verticalIndex = j;
+				break;
+			}
+		}
+	}
+	int nOfChildren = childrenCalculator(x);
+	bool left = true;
+	bool right = true;
+	bool up = true;
+	bool down = true;
+	for(int i = 0; i < x->illegalMoves.size(); i++){
+		if(x->illegalMoves.at(i) == 1){
+			left = false;
+		}
+		else if(x->illegalMoves.at(i) == 2){
+			right = false;
+		}
+		if(x->illegalMoves.at(i) == 3){
+			up = false;
+		}
+		else if(x->illegalMoves.at(i) == 4){
+			down = false;
+		}
+	}
+
+	if(left){
+		leftShift = moveLeft(x,horizontalIndex,verticalIndex);
+		assignChild(x, leftShift, x->child1);
+	}
+	if(right){
+		rightShift = moveRight(x,horizontalIndex,verticalIndex);
+		assignChild(x, rightShift, x->child2);
+	}
+	if(up){
+		upShift = moveUp(x,horizontalIndex,verticalIndex);
+		assignChild(x, upShift, x->child3);
+	}
+	if(down){
+		downShift = moveDown(x,horizontalIndex,verticalIndex);
+		assignChild(x, downShift, x->child4);
+	}
+
+	if(goalState){
+		while(q.top() != goalNode)
+		{
+			q.pop();
+		}
+		displayPuzzle(q.top());
+		cout << "Goal!" << endl;
+
+		cout << "To solve this problem the search algorithm expanded a total of ";
+		cout << totalNodes << " nodes." << endl;
+
+		cout << "The maximum nodes in the queue at any one time was ";
+		cout << nodesInQueue << "." << endl;
+
+		cout << "The depth of the goal node was ";
+		cout << q.top()->branchCost << "." << endl;
+
+		return;
+	}
+
+	//Left = 1
+	//Right = 2
+	//Up = 3
+	//Down = 4
+
+	manhattanDistanceSearch(q.top());
 }
 
 vector< vector<char> > puzzle::moveLeft(path* x,int i,int j){
@@ -382,9 +684,10 @@ void puzzle::assignChild(path* x, vector< vector<char> > puzzle, path* child){
 path* puzzle::childCreator(path* x, vector< vector<char> > puzzle){
 	path* temp = new path;
 
-
+	temp->heuristic = x->heuristic;
 	temp->branchCost = x->branchCost + 1;
 	temp->puzzle = puzzle;
+
 
 	return temp;
 }
